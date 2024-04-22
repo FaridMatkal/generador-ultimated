440efb828c834c03807d0dd583588d3677329d44
FaridMatkal <juanvalenzuelacamelo@gmail.com>
2024-04-22 11:34:06 -0500

    Proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,40 +7,59 @@ import { QuoteComponent } from './Citas.jsx';
 
 {/* npm install react-icons (único comando para instalar la librería de react)*/}
 
+
+
 function App() {
   
-  const quoteGenerator = QuoteComponent();
   
+  const [currentQuote, setCurrentQuote] = useState(QuoteComponent());
+  const [pageColor, setPageColor] = useState('orange');
+
+  const colors = [
+    '#8a2be2',
+    '#3d636e',
+    '#862b24',
+    '#746f6f',
+    '#297429'
+  ];
+
+  const generateNewColor = () => {
+    let newColor;
+    do {
+      const randomIndex = Math.floor(Math.random() * colors.length);
+      newColor = colors[randomIndex];
+    } while (newColor === pageColor); // Repetir si el nuevo color es igual al color actual
+    setPageColor(newColor);
+    setCurrentQuote(QuoteComponent()); // Genera la nueva cita
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = pageColor; // Cambia el color de fondo del body
+  }, [pageColor]);
 
   return (
-    <div id='quote-box'>
+    <div id='quote-box' >
 
     <div className='text'>
-      <i className='comilla'>
+      <i className='comilla' style={{ color: pageColor }}>
         <FaQuoteLeft/>
       </i>
-        <span id='text'> {quoteGenerator.cita} </span>
-      <i className='comilla'>
+        <span id='text' style={{ color: pageColor }}> {currentQuote.cita} </span>
+      <i className='comilla' style={{ color: pageColor }}>
         <FaQuoteRight/>
       </i>
       
     </div>
 
-    <div className='author'>  
-      <span id='author'> - {quoteGenerator.author} </span>
+    <div className='author' style={{ color: pageColor }}>  
+      <span id='author'> - {currentQuote.author} </span>
     </div>
 
     <div className='buttons'>
-      <a id='tweet-quote' href="">
-        <i className='social-media'><FaTwitterSquare/></i>
+      <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(currentQuote.cita)} - ${encodeURIComponent(currentQuote.author)}`}>
+        <i className='social-media' style={{ color: pageColor }}><FaTwitterSquare/></i>
       </a>
-      <a href="">
-        <i className='social-media'><FaInstagramSquare/></i>
-      </a>
-      <a href="">
-        <i className='social-media'><FaFacebookSquare/></i>
-      </a>
-      <button id='new-quote'>New quote</button>
+      <button id='new-quote' onClick={() => { generateNewColor(); setCurrentQuote(QuoteComponent()); }} style={{ backgroundColor: pageColor }}>New quote</button>
     </div>
   </div>
   )
